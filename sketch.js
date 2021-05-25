649547b1bf4dad057edc3bff34505286eab8332c
alajillo <alajillo2021@gmail.com>
2021-05-25 21:17:34 +0900

    p5.js

---
+++ sketch.js
@@ -0,0 +1,41 @@
+let raindrops = [];
+let canvas;
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style("z-index", "-1");
+  fill("blue");
+  noStroke();
+}
+
+function draw() {
+  background(240);
+
+  for (let i = 0; i < random(10); i++) {
+    raindrops.push(new Raindrop());
+  }
+  for (let rain of raindrops) {
+    rain.update();
+    rain.display();
+  }
+}
+
+function Raindrop() {
+  this.posX = random(width);
+  this.posY = random(-20, 0);
+  this.size = random(10, 30);
+
+  this.update = function () {
+    this.posY += pow(this.size, 0.75) * 2;
+  };
+
+  if (this.posY > height) {
+    let index = raindrops.indexOf(this);
+    raindrops.splice(index, 1);
+  }
+
+  this.display = function () {
+    rect(this.posX, this.posY, 2, this.size);
+  };
+}
